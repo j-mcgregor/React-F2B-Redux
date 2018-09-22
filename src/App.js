a4bb11ae6973473237ae97ccd1e41c8b460c29ad
patchyj <jackjwmcgregor@gmail.com>
2018-09-22 12:13:37 +0100

    added basename

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <div className="App">
             <Header branding="React F2B w/ Redux" />
               <Switch>
